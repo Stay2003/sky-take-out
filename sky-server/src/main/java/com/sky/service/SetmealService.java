ec3ffc71f2ee41573d1d69ef4e2c6ddb414e2058
stay2003 <3107479638@qq.com>
2023-11-16 11:27:58 +0800

    回显,删除,修改套餐业务代码开发

--- sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,13 @@
 package com.sky.service;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.enumeration.OperationType;
+import com.sky.result.PageResult;
+import com.sky.vo.SetmealVO;
+
+import java.util.List;
 
 public interface SetmealService {
 
@@ -9,4 +16,39 @@ public interface SetmealService {
      * @param setmealDTO
      */
     void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     */
+    void deleteBatch(List<Long> ids);
+
+    /**
+     * 根据id查询套餐和关联的菜品数据
+     * @param id
+     * @return
+     */
+    SetmealVO getByIdWithDish(Long id);
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     */
+    @AutoFill(value = OperationType.UPDATE)
+    void update(SetmealDTO setmealDTO);
+
+
+    /**
+     * 套餐起售、停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
 }
